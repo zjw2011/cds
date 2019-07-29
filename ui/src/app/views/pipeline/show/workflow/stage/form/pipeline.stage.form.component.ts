7b64cac18efa9500e36545e699aa10c9a268bb78
Steven Guiheux <steven.guiheux@corp.ovh.com>
2019-07-29 10:09:37 +0200

    remove old permission object

--- ui/src/app/views/pipeline/show/workflow/stage/form/pipeline.stage.form.component.ts
+++ ui/src/app/views/pipeline/show/workflow/stage/form/pipeline.stage.form.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { WorkflowTriggerConditionCache } from 'app/model/workflow.model';
 import { PipelineService } from 'app/service/pipeline/pipeline.service';
 import { finalize } from 'rxjs/operators';
-import { PermissionValue } from '../../../../../../model/permission.model';
 import { Pipeline } from '../../../../../../model/pipeline.model';
 import { Project } from '../../../../../../model/project.model';
 import { Stage } from '../../../../../../model/stage.model';
@@ -17,8 +16,6 @@ export class PipelineStageFormComponent implements OnInit {
     @Input() project: Project;
     @Input() pipeline: Pipeline;
     @Input() stage: Stage;
-
-    permissionEnum = PermissionValue;
     triggerConditions: WorkflowTriggerConditionCache;
     loading = true;
 
