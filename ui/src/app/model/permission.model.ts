b57ce7bd521965fecdd1f805abfc485540adcf3d
Steven Guiheux <steven.guiheux@corp.ovh.com>
2019-08-27 14:41:37 +0200

    fix ui

--- ui/src/app/model/permission.model.ts
+++ ui/src/app/model/permission.model.ts
@@ -3,9 +3,3 @@ export class Permission {
     executable: boolean;
     writable: boolean;
 }
-
-export class Permission {
-    readable: boolean;
-    executable: boolean;
-    writable: boolean;
-}
